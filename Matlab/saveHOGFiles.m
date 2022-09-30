fe153755f465958ac36e88e843dfe9283ca32f29
Edwin Joseph <ejsongbank@gmail.com>
2022-09-30 16:46:04 +0530

    Data for 1-4

---
+++ Matlab/saveHOGFiles.m
@@ -0,0 +1,15 @@
+function featureVector = saveHOGFiles(fileName, saveAs)
+
+    image = imread(fileName);
+    
+%     [featureVector,hogVisualization] = extractHOGFeatures(image);
+    featureVector = extractHOGFeatures(image);
+    
+%     figure;
+%     imshow(image); 
+%     hold on;
+%     plot(hogVisualization);
+    
+    T = table(featureVector);
+    writetable(T, saveAs, 'WriteVariableNames', false);
+end
